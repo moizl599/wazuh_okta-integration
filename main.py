d8b403a7c4dc0f50ad966207519b2b8cefceab70
Zoim <42458629+moizl599@users.noreply.github.com>
2023-03-03 14:50:35 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ config = ConfigParser()
 config.read('./key.cfg')
 API_KEY = config.get('SSWS_Key', 'key')
 thirty_time = datetime.datetime.now() - datetime.timedelta(minutes=30)
-url = f"https://okta.workjam.com/api/v1/logs?since={thirty_time.isoformat()}"
+url = f"https://<okta domain>/api/v1/logs?since={thirty_time.isoformat()}"
 file = config.get('Log_file', 'file')
 payload = {}
 headers = {
@@ -23,7 +23,6 @@ if os.path.isfile(file):
     os.remove(file)
 with open(file, 'x', encoding='utf-8') as f:
     for log in raw_log_list:
-        print (log['displayMessage'])
         with open(file, 'a', encoding='utf-8') as Log_file:
             log['Log_type'] = 'Okta'
             json.dump(log, Log_file, ensure_ascii=False)
