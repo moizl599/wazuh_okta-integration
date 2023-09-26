a7f6eb56464ea1add1da4bc5a2808977f1e4531f
Zoim <42458629+moizl599@users.noreply.github.com>
2023-09-26 12:44:56 -0400

    rewrote according to pep8 standards

--- main.py
+++ main.py
@@ -1,28 +1,47 @@
-import requests
+import os
 import json
 import datetime
-import os
 from configparser import ConfigParser
 
-config = ConfigParser()
-config.read('./key.cfg')
-API_KEY = config.get('SSWS_Key', 'key')
-thirty_time = datetime.datetime.now() - datetime.timedelta(minutes=30)
-url = f"https://<okta domain>/api/v1/logs?since={thirty_time.isoformat()}"
-file = config.get('Log_file', 'file')
-payload = {}
-headers = {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json',
-    'Authorization': API_KEY
-}
-response = requests.request("GET", url, headers=headers, data=payload)
-raw_log_list = json.loads(response.text)
-if os.path.isfile(file):
-    os.remove(file)
-with open(file, 'x', encoding='utf-8') as f:
-    for log in raw_log_list:
-        with open(file, 'a', encoding='utf-8') as Log_file:
-            log['Log_type'] = 'Okta'
-            json.dump(log, Log_file, ensure_ascii=False)
-            Log_file.write('\n')
+import requests
+
+
+class OktaLogger:
+    def __init__(self):
+        self.config = ConfigParser()
+        self.config.read('./key.cfg')
+        self.api_key = self.config.get('SSWS_Key', 'key')
+        self.thirty_time = datetime.datetime.now() - datetime.timedelta(minutes=30)
+        self.url = f"https://<okta domain>/api/v1/logs?since={self.thirty_time.isoformat()}"
+        self.file = self.config.get('Log_file', 'file')
+        self.headers = {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': self.api_key
+        }
+
+    def get_logs(self):
+        response = requests.get(self.url, headers=self.headers)
+        if response.status_code == 200:
+            return json.loads(response.text)
+        else:
+            response.raise_for_status()
+
+    def write_logs(self, logs):
+        if os.path.isfile(self.file):
+            os.remove(self.file)
+
+        with open(self.file, 'w', encoding='utf-8') as log_file:
+            for log in logs:
+                log['Log_type'] = 'Okta'
+                json.dump(log, log_file, ensure_ascii=False)
+                log_file.write('\n')
+
+    def execute(self):
+        logs = self.get_logs()
+        self.write_logs(logs)
+
+
+if __name__ == "__main__":
+    logger = OktaLogger()
+    logger.execute()
